b2d24fdaef60ef3ef711ff7c08860b81c30716b3
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-04 12:22:08 +0100

    DONE: partie5-exo1

---
+++ algo.php
@@ -0,0 +1,18 @@
+<?php 
+
+/* Exercise 1 
+$months = [
+    "janvier",
+    "fevrier",
+    "mars",
+    "avril",
+    "mai",
+    "juin",
+    "juillet",
+    "aout",
+    "septembre",
+    "octobre",
+    "novembre",
+    "decembre"
+];
+*/
